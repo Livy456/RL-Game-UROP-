43d48e58930f676eed7ad6c3d59b021eff786b3d
Olivia <52472697+Livy456@users.noreply.github.com>
2024-01-24 09:41:27 -0500

    updated how the actions are being retrieved from qtable

--- RAICA UROP/reinforcementLearning.js
+++ RAICA UROP/reinforcementLearning.js
@@ -3,20 +3,16 @@ class reinforcementLearning
     // constructor(states, actions, learning_rate, gamma, rewards)
     constructor()
     {
-        // ITS NOT GETTING INSIDE THE reinforcementLearning constructor method
-        // document.write("in constructor of reinforcement learning");
-
-
-
-
-        // this.states = this.#getStates();                       // array of possible states for the car
-        // this.actions = this.#getActions();                     // array of possible actions for the car
+        // document.write("inside the reinforcement learning function!!");
+        // this.states = this.#getState(5);                       // array of possible states for the car
+        this.actions = this.#getActions(5);                     // array of possible actions for the car
+        // document.write("I have retrieved the states and actions");
         this.states = ["Car Detected", "Road Border Detected", "Collision", "Nothing Detected"];
-        this.actions = ["Forward", "Backward", "Left", "Right", "Stop"];
+        // this.actions = ["Forward", "Backward", "Left", "Right", "Stop"];
         
         this.learning_rate = this.#getLearningRate();         // learning rate for the q learning process
         this.gamma = 0.9;   // NEED TO FIGURE OUT WHAT THIS VALUE IS SUPPOSED TO BE
-        this.reward_matrix = rewards;               // 2D matrix of all state, action pairings
+        this.reward_matrix = this.#getRewards;               // 2D matrix of all state, action pairings
         this.qTable = this.#createQTable();         // initializes an all zero 2D matrix for qTable
         this.policy = new Map();                    // empty mapping of optimal action for car to take given a state
         this.current_action = "forward";            // current action for player car to take
@@ -39,11 +35,40 @@ class reinforcementLearning
         // what action to take if multiple things are sensed/detected
     }
 
-    #getActions()
+    #getActions(num_actions)
+    {
+        // document.write("entered get actions function in reinforcement learning class!!");
+        let actions = [];
+        // let table = document.getElementById("q_table");
+
+        // loop through each column of table to get the actions for the game
+        for (let index = 0; index < num_actions; index++)
+        {
+            let action_number = index + 1
+            let id_name = "action" + action_number.toString(); // figure out if this is the accurate
+            let table = document.getElementById(id_name);
+            
+            let data = table.innerHTML;
+            let data_element = "";
+
+    
+            for (let i = 0; i < data.length; i++)
+            {
+                const data_value = data[i];
+                data_element = data_element + data_value;
+            }
+
+            actions.push(data_element);
+        }
+
+        return actions;
+    }
+
+    #getState(num_states)
     {
         let table = document.getElementById("q_table");
-        let cell = table.getElementById("th");
-        let website_document = document.getElementById("simulation");
+        // let cell = table.getElementById("th");
+        // let website_document = document.getElementById("simulation");
 
         for (let i=0; i<cell.length;i++)
         {
@@ -54,45 +79,34 @@ class reinforcementLearning
             // if (type())
             // need to check the type of data value
             // since data value could be an integer and can't compare string to integer
-            if ("State, Action" === data_value)
+            // if ("State, Action" === data_value)
+            // {
+            //     continue;
+            // } 
+
+            // check if you are at the end of a word
+            if ( (" " === data_value) || ("" === data_value) )
             {
+                // checks to make sure you're adding a word
+                if (data_element.length !== 0)
+                {
+                    actions.push(data_element);
+                    data_element = "";
+                }
                 continue;
-            } 
+            }
 
  
         }
         // website_document.innerHTML = "I can write to the website";
     }
 
-    #getStates()
-    {
-        let table = document.getElementById("q_table");
-        let cell = table.getElementById("tr");
-        let website_document = document.getElementById("simulation");
-
-        for (let i=0; i<cell.length;i++)
-        {
-            const data_value = cell[i].innerHTML;
-            document.write(i, "th data value: ", data_value);
-            
-            
-            // if (type())
-            // need to check the type of data value
-            // since data value could be an integer and can't compare string to integer
-            if ("State, Action" === data_value)
-            {
-                continue;
-            } 
-
- 
-        }
-    }
-
     #getRewards()
     {
-        let table = document.getElementById("q_table");
+        let table = document.getElementById("reward_table");
         let cell = table.getElementById("tr");
-        let website_document = document.getElementById("simulation");
+        let rewards = [];
+        // let website_document = document.getElementById("simulation");
 
         for (let i=0; i<cell.length;i++)
         {
@@ -106,32 +120,35 @@ class reinforcementLearning
             if ("State, Action" === data_value)
             {
                 continue;
-            } 
-
- 
+            }
         }
+
+        return rewards
     }
 
+    // SUCCESSFULLY GETS THE LEARNING RATE OF THE SLIDER
     #getLearningRate()
     {
-        document.write("I am in get the learning rate function!!!");
         let slider = document.getElementById("lr_slider");
-        let website_document = document.getElementById("simulation"); // need to determine if this is the accurate name 
+        // let website_document = document.getElementById("simulation"); // need to determine if this is the accurate name 
                                                                       // might have to be document title or a div container id name
+        
+        // website_document is undefined, wrong id name
         const learning_rate = slider.value / 100;
-        document.write("I am got the learning rate!!!");
 
         // get learning rate from slider
         return learning_rate
     }
-    
 
     #updateValues()
     {
         // updates learning rate based on slider value
-        let website_document = document.getElementById("simulation");
+        // let website_document = document.getElementById("simulation");
         let slider = document.getElementById("lr_slider");
-        this.learning_rate = slider.value;
+        this.learning_rate = slider.value / 100;
+
+        // will need to use 
+        // .getElementById("").innerHTML = value
 
         slider.oninput = function(){
 
@@ -158,6 +175,7 @@ class reinforcementLearning
         return q_matrix;
     }
 
+    // need to make a time step function to be able to make a time step to the next state
     updateState(sensedObject)
     {
         // objects that can be detected!!
@@ -231,8 +249,7 @@ class reinforcementLearning
             this.qTable[row][col] = (1 - this.learning_rate) * current_q_value +
                                     this.learning_rate* (reward + this.gamma);
         }
-        //     }   
-        // }   
+       
     }
 
     // Later add in a function that makes a txt file with
